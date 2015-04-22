0553b188c59e68f524678da6f0bc9413529d7d1c
Firestack <me@stackfire.io>
2015-04-22 11:13:09 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'static#index'
+  
+  get '/about', to: 'static_pages#about', as: :about
+  
+  get 'util/authentication', to: 'static_pages#util#auth', as: :AppAuth
 end

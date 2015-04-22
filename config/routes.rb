567ae5bc56cb7ce5a53ed79e06d6ca1a87fba7cf
Firestack <me@stackfire.io>
2015-04-22 16:17:23 -0600

    T_T

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static#index'
   
-  get '/about', to: 'static#about', as: :about
+  get '/about', to: 'static#about', as: :About
   
   get 'util/authentication', to: 'static#util#auth', as: :AppAuth
 end

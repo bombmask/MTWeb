db236e3b74786db9bcb9dfb0c7acb89d3ec23b55
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2015-04-30 03:49:59 -0700

    shell for channels

---
+++ app/controllers/channels_controller.rb
@@ -0,0 +1,10 @@
+class ChannelsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'static#index'
 
-  get '/about',       to: 'static#about', as: :about
+  get '/about',           to: 'static#about',   as: :about
 
-  get '/utils/login',  to: 'twitch#out',   as: :twitch_out
-  get '/utils/auth',   to: 'twitch#in',    as: :twitch_in
+  get '/utils/login',     to: 'twitch#out',     as: :twitch_out
+  get '/utils/auth',      to: 'twitch#in',      as: :twitch_in
+
+  get '/channels',        to: 'channels#index', as: :channels
+  get '/channels/:name',  to: 'channels#show'
 
 end
 

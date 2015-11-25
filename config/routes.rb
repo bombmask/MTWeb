76fe91e895da0c84d89a09cb7bc05ed3d91b997e
Firestack <me@stackfire.io>
2015-11-25 01:57:48 -0700

    my VM isn't here

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/utils/login',     to: 'twitch#out',     as: :twitch_out
   get '/utils/auth',      to: 'twitch#in',      as: :twitch_in
 
-  get '/app'              to: 'app#index'       as: :app
+  get '/app',             to: 'app#index',      as: :app
   get '/channels',        to: 'channels#index', as: :channels
   get '/channels/:name',  to: 'channels#show'
 
